d0f6f2686a51e08cf679bbbc922f92d138d74c5f
Allan Imire <imireallan@gmail.com>
2018-05-30 00:14:00 +0300

    modified init

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ db = SQLAlchemy()
 
 def create_app(config_name):
     """Instantiating the app and its configurations"""
-    from models import Bucketlist
+    from app.models import Bucketlist
     app = FlaskAPI(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile("config.py")
