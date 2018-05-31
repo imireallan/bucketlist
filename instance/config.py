645283b12bd39da17acbbf8fddf509ccd03ac621
Allan Imire <imireallan@gmail.com>
2018-05-31 20:13:52 +0300

    modified config

--- instance/config.py
+++ instance/config.py
@@ -19,7 +19,7 @@ class ProductonConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/test_db"
+    SQLALCHEMY_DATABASE_URI = "postgresql://imire:pass@word1@localhost/test"
 
 app_config = {
     "development": DevelopmentConfig,
